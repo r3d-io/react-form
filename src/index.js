2d0442db4f77e570cdbcddcd64ca61d524167d49
Anuj singh <askoli@techracers.io>
2019-08-01 17:03:48 +0530

    Edit is working with minor bugs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 class InsertUser extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: '', age:'' };
+    this.state = { name: this.props.userDetail.name, age: this.props.userDetail.age };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleNumChange = this.handleNumChange.bind(this);
+  }
+
+  componentWillReceiveProps() {
+    if (this.props.editUserStatus) {
+      this.setState({ name: this.props.userDetail.name, age: this.props.userDetail.age });
+    }
+    else {
+      this.setState({ name: '', age: '' });
+    }
   }
 
   handleTextChange(event) {
@@ -24,33 +33,56 @@ class InsertUser extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     this.props.addUserToList({ name: this.state.name, age: this.state.age });
-    this.setState({ name: ' ' , age: ''});
+    this.setState({ name: '', age: '' });
+    this.props.resetPersonalData();
+    console.log(this.state)
   }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={this.handleTextChange}
-          />
-        </label>
-        <br></br>
-        <label>
-          Age:
-          <input
-            type="number"
-            value={this.state.age}
-            onChange={this.handleNumChange}
-          />
-        </label>
-        <br></br>
-        <input
-          type="submit"
-          value="Submit"
-        />
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <h4> Name:</h4>
+              </td>
+              <td>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.handleTextChange}
+                  />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4> Age:</h4>
+              </td>
+              <td>
+                <div className="form-group">
+                  <input
+                    type="number"
+                    value={this.state.age}
+                    onChange={this.handleNumChange}
+                  />
+                </div>
+              </td>
+            </tr>
+            <tr><td>
+              <div className="form-group">
+                <input
+                  className="btn btn-success"
+                  type="submit"
+                  value="Submit"
+                />
+              </div>
+            </td>
+            </tr>
+          </tbody>
+        </table>
       </form>
     );
   }
@@ -60,20 +92,62 @@ class ListElement extends React.Component {
   render() {
     return (
       <tr>
-        <td>{this.props.index}</td>        
+        <td>{this.props.index}</td>
         <td>{this.props.name}</td>
         <td>{this.props.age}</td>
+        <td>{this.props.edit}</td>
+        <td>{this.props.delete}</td>
       </tr>
     );
   }
 };
 
 class ShowList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.updateValue = this.updateValue.bind(this);
+  }
+
+  updateValue(event, userIndex, actionType) {
+    event.preventDefault();
+    this.props.editUserDetail({ index: userIndex, type: actionType });
+  }
+
+  editUser(index) {
+    return (
+      <button
+        type='submit'
+        value={index}
+        onClick={(event) => this.updateValue(event, index, 'update')}
+        className="btn btn-primary"
+      >Edit</button>
+    )
+  }
+
+  deleteUser(index) {
+    return (
+      <button
+        type='submit'
+        value={index}
+        onClick={(event) => this.updateValue(event, index, 'delete')}
+        className="btn btn-danger"
+      >Delete</button>
+    )
+  }
+
   render() {
     let list = [];
 
     this.props.list.forEach((user, index) => {
-      list.push(<ListElement index={index+1} key={index} name={user.name} age={user.age} />);
+      list.push(<ListElement
+        index={index + 1}
+        key={index}
+        name={user.name}
+        age={user.age}
+        edit={this.editUser(index)}
+        delete={this.deleteUser(index)}
+      />);
     })
 
     return (
@@ -81,17 +155,11 @@ class ShowList extends React.Component {
         <h1>User Detail</h1>
         <table>
           <tbody>
-          <tr>
-            <th>
-              SNo.
-          </th>
-            <th>
-              Name
-          </th>
-            <th>
-              Age
-          </th>
-          </tr>
+            <tr>
+              <th>SNo.</th>
+              <th>Name</th>
+              <th>Age</th>
+            </tr>
             {list}
           </tbody>
         </table>
@@ -103,24 +171,62 @@ class UserForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userList: [{ name: "foo", age: "20"}, { name: "bar", age: "25" }]
+      userList: [{ name: "foo", age: "20" }, { name: "bar", age: "25" }],
+      editUserStatus: false,
+      userDetail: { name: '', age: '' }
     };
     this.setUser = this.setUser.bind(this)
+    this.editUser = this.editUser.bind(this)
+    this.resetPersonalData = this.resetPersonalData.bind(this)
   }
 
   setUser(data) {
-    this.state.userList.push(data)
-    // alert(this.state.userList)
+    if (this.state.editUserStatus) {
+      this.state.userList[this.state.userIndex] = data
+      this.setState({
+        userList: this.state.userList,
+        editUserStatus: false,
+      });
+    }
+    else {
+      if (data.name === '' || data.age === '')
+        alert("Please fill the other field")
+      if (data.name !== '' && data.age !== '') {
+        this.state.userList.push(data)
+        this.setState({
+          userList: this.state.userList
+        });
+      }
+    }
+  }
+
+  editUser(data) {
     this.setState({
-      userList: this.state.userList
+      editUserStatus: true,
+      userDetail: this.state.userList[data.index],
+      userIndex: data.index
+    });
+  }
+
+  resetPersonalData() {
+    this.setState({
+      userDetail: { name: '', age: '' }
     });
   }
 
   render() {
     return (
       <div>
-        <InsertUser addUserToList={this.setUser} />
-        <ShowList list={this.state.userList} />,
+        <InsertUser
+          addUserToList={this.setUser}
+          editUserStatus={this.state.editUserStatus}
+          userDetail={this.state.userDetail}
+          resetPersonalData={this.resetPersonalData}
+        />
+        <ShowList
+          list={this.state.userList}
+          editUserDetail={this.editUser}
+        />,
 			</div>
     );
   }
@@ -132,4 +238,5 @@ ReactDOM.render(<UserForm />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-// https://codepen.io/PiotrBerebecki/pen/dpRdKP?editors=0010
+        // https://codepen.io/PiotrBerebecki/pen/dpRdKP?editors=0010
+// https://www.codingame.com/playgrounds/8747/react-lifecycle-methods-render-and-componentdidmount
